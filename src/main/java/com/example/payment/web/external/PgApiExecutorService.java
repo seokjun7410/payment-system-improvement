7a33c6a7a7e3ed4ef7e2f3b10ad3bd38bf69db94
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-03-13 00:48:58 +0900

    anno: PgApiExecutorService 멱등성 보장 가정 주석 추가

--- src/main/java/com/example/payment/web/external/PgApiExecutorService.java
+++ src/main/java/com/example/payment/web/external/PgApiExecutorService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PgApiExecutorService {
+	/** 사용하는 PG에 따라 맞는 멱등성 보장 방법을 구현해야 합니다. 단순화를 위해 생략합니다. **/
 	public PaymentResponse mockPaymentApiCall(PaymentRequest request) {
 		PaymentResponse response = new PaymentResponse();
 		// 외부 PG API 호출 대신 항상 성공으로 모킹 (필요 시 실패 시나리오 구현 가능)
